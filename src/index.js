3d3f97429c5400e5cdbe59641d7b7d22ef876ef6
hy <zihong2020@foxmail.com>
2021-06-10 09:27:25 +0800

    feat(all): start Base Webpack5

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-10 09:03:09
+ * @LastEditTime: 2021-06-10 09:16:13
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /webpack5/src/index.js
+ */
+console.log(1);
+/*
+
+> webpack5-study@0.0.1 build
+> webpack
+
+asset main.js 428 bytes [emitted] (name: main) 1 related asset
+./src/index.js 228 bytes [built] [code generated]
+webpack 5.38.1 compiled successfully in 98 ms
+
+*/ 
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2021-06-10 08:47:13
- * @LastEditTime: 2021-06-10 08:56:42
+ * @LastEditTime: 2021-06-10 09:24:53
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /webpack5/webpack.config.js
  */
 const path = require('path')
 module.exports = {
-  mode:'development',
+  mode:'none',// 模式 [development / production / none]
   devtool:'source-map',
   context:process.cwd(),//工作目录
   entry:'./src/index.js',// 入口起点 默认值是 ./src/index.js
